359beb2ac866cd97f20a85c1d2930fdeb2dea8a1
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-02-27 11:45:54 -0800

    gallery functionality added

--- js/index.js
+++ js/index.js
@@ -1,35 +1,70 @@
 // window.addEventListener('load', () => {
-  const gallery = document.getElementsByClassName('gallery')[0];
-  const galleryItems = document.getElementsByClassName('gallery__item');
-  
-  /* HTMLCollection [
-    <figure class="gallery__item">
-      <img 
-        class="gallery__item--img" 
-        src="img/jeopardy.jpeg" 
-        alt="Jeopardy image" 
-        />
-      <figcaption class="gallery__description">Jeopardy</figcaption>
-    </figure> ]
-    */
-  
-gallery.addEventListener('click', (ele) => { 
-  const target = ele.target;
-  let imgEl;
+const GALL_TOP = 'gallery--top';
+const GALL_BOTTOM = 'gallery--bottom';
+const ACTIVE = 'active';
+const GALLERY = document.getElementById('gallery');
+const GALLERY_IMAGES = document.getElementsByClassName('gallery__item--img');
 
+let activeImg = document.getElementsByClassName(ACTIVE)[0];
+let topImg = document.getElementsByClassName(GALL_TOP)[0];
+let bottomImg = document.getElementsByClassName(GALL_BOTTOM)[0];
+
+// const galleryItems = document.getElementsByClassName('gallery__item');
+  
+GALLERY.addEventListener('click', (ele) => { 
+  const target = ele.target;
   
   if(target === undefined) return;
 
   switch(target.tagName) {
     case 'IMG' : {
-      console.log('IMAGE HIT');
-      console.log(Array.from(galleryItems));
+      const PARENT = target.parentElement;
+      const IS_ACTIVE = PARENT.classList.contains(ACTIVE);
+
+      if(IS_ACTIVE) return;
+      if(PARENT.classList.contains(GALL_TOP)) {
+        topImg.classList.replace(GALL_TOP, ACTIVE);
+        activeImg.classList.replace(ACTIVE, GALL_TOP);
+        
+        // swap elements
+        [activeImg, topImg] = [topImg, activeImg];        
+      } else {
+        bottomImg.classList.replace(GALL_BOTTOM, ACTIVE);
+        activeImg.classList.replace(ACTIVE, GALL_BOTTOM);
+        
+        // swap elements
+        [activeImg, bottomImg] = [bottomImg, activeImg]; 
+      }
       break;
     }
     default: 
       console.log('NO_EVENTS_EXIST');
   }
 });
-  // console.log('GALLERY', gallery);
-  // console.log('GALLERY_ITEMS >>> ', galleryItems);
+
+GALLERY.addEventListener('mouseover', (ele) => {
+  const target = ele.target;
+  // console.log('MOUSE_OVER:::TARGET', target)
+  let isActive = target.parentElement.classList.contains('active');
+  if(target.tagName !== 'IMG' || target.tagName === undefined) return;
+
+  if(!isActive) {
+    // console.log('OPACITY-100 ADDED')
+    target.classList.replace('opacity-50', 'opacity-100');
+  };
+});
+
+for(img of GALLERY_IMAGES) {
+  img.addEventListener('mouseleave', (ele) => {
+    const target = ele.target;
+
+    if(target.tagName !== 'IMG' || target.tagName === undefined) return;
+
+    const topImg = target.parentElement.classList.contains(GALL_TOP);
+    const bottomImg = target.parentElement.classList.contains(GALL_BOTTOM);
+    
+    if(topImg || bottomImg) target.classList.replace('opacity-100', 'opacity-50');
+
+  });
+}
 // })
