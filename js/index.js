e5667c2cd196a851f3efc4669294257fefe0fe15
eduardo <eduardoaviles357@yahoo.com>
2024-03-09 08:13:21 -0800

    removed comment

--- js/index.js
+++ js/index.js
@@ -9,35 +9,33 @@ let activeImg = document.getElementsByClassName(ACTIVE)[0];
 let topImg = document.getElementsByClassName(GALL_TOP)[0];
 let bottomImg = document.getElementsByClassName(GALL_BOTTOM)[0];
 
-// const galleryItems = document.getElementsByClassName('gallery__item');
-  
-GALLERY.addEventListener('click', (ele) => { 
+GALLERY.addEventListener('click', (ele) => {
   const target = ele.target;
-  
-  if(target === undefined) return;
 
-  switch(target.tagName) {
-    case 'IMG' : {
+  if (target === undefined) return;
+
+  switch (target.tagName) {
+    case 'IMG': {
       const PARENT = target.parentElement;
       const IS_ACTIVE = PARENT.classList.contains(ACTIVE);
 
-      if(IS_ACTIVE) return;
-      if(PARENT.classList.contains(GALL_TOP)) {
+      if (IS_ACTIVE) return;
+      if (PARENT.classList.contains(GALL_TOP)) {
         topImg.classList.replace(GALL_TOP, ACTIVE);
         activeImg.classList.replace(ACTIVE, GALL_TOP);
-        
+
         // swap elements
-        [activeImg, topImg] = [topImg, activeImg];        
+        [activeImg, topImg] = [topImg, activeImg];
       } else {
         bottomImg.classList.replace(GALL_BOTTOM, ACTIVE);
         activeImg.classList.replace(ACTIVE, GALL_BOTTOM);
-        
+
         // swap elements
-        [activeImg, bottomImg] = [bottomImg, activeImg]; 
+        [activeImg, bottomImg] = [bottomImg, activeImg];
       }
       break;
     }
-    default: 
+    default:
       console.log('NO_EVENTS_EXIST');
   }
 });
@@ -46,24 +44,24 @@ GALLERY.addEventListener('mouseover', (ele) => {
   const target = ele.target;
   // console.log('MOUSE_OVER:::TARGET', target)
   let isActive = target.parentElement.classList.contains('active');
-  if(target.tagName !== 'IMG' || target.tagName === undefined) return;
+  if (target.tagName !== 'IMG' || target.tagName === undefined) return;
 
-  if(!isActive) {
+  if (!isActive) {
     // console.log('OPACITY-100 ADDED')
     target.classList.replace('opacity-50', 'opacity-100');
   };
 });
 
-for(img of GALLERY_IMAGES) {
+for (img of GALLERY_IMAGES) {
   img.addEventListener('mouseleave', (ele) => {
     const target = ele.target;
 
-    if(target.tagName !== 'IMG' || target.tagName === undefined) return;
+    if (target.tagName !== 'IMG' || target.tagName === undefined) return;
 
     const topImg = target.parentElement.classList.contains(GALL_TOP);
     const bottomImg = target.parentElement.classList.contains(GALL_BOTTOM);
-    
-    if(topImg || bottomImg) target.classList.replace('opacity-100', 'opacity-50');
+
+    if (topImg || bottomImg) target.classList.replace('opacity-100', 'opacity-50');
 
   });
 }
