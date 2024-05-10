85641799ea1e9e7e21775af3bcb470c60eb75fdf
eduardo <eduardoaviles357@yahoo.com>
2024-05-09 20:16:34 -0700

    intersection observer added to observe containers, once containers are in view we apply a class to create an illusion of fadig into view

--- js/index.js
+++ js/index.js
@@ -1,68 +1,27 @@
 // // window.addEventListener('load', () => {
-// const GALL_TOP = 'gallery--top';
-// const GALL_BOTTOM = 'gallery--bottom';
-// const ACTIVE = 'active';
-// const GALLERY = document.getElementById('gallery');
-// const GALLERY_IMAGES = document.getElementsByClassName('gallery__item--img');
+window.addEventListener('DOMContentLoaded', (evt) => {
+    const options = {
+        root: null,
+        threshold: 0,
+        rootMargin: '0px 0px -50px 0px'
+    };
 
-// let activeImg = document.getElementsByClassName(ACTIVE)[0];
-// let topImg = document.getElementsByClassName(GALL_TOP)[0];
-// let bottomImg = document.getElementsByClassName(GALL_BOTTOM)[0];
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            console.log(entry.isIntersecting)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('scroll-in-view');
+                // observer.unobserve(entry.target);
+            }
+        });
+    }, options);
 
-// GALLERY.addEventListener('click', (ele) => {
-//   const target = ele.target;
 
-//   if (target === undefined) return;
+    const sections = Array.from(document.getElementsByClassName('container'));
+    console.log(sections)
+    for (let section of sections) {
+        observer.observe(section);
+    }
 
-//   switch (target.tagName) {
-//     case 'IMG': {
-//       const PARENT = target.parentElement;
-//       const IS_ACTIVE = PARENT.classList.contains(ACTIVE);
-
-//       if (IS_ACTIVE) return;
-//       if (PARENT.classList.contains(GALL_TOP)) {
-//         topImg.classList.replace(GALL_TOP, ACTIVE);
-//         activeImg.classList.replace(ACTIVE, GALL_TOP);
-
-//         // swap elements
-//         [activeImg, topImg] = [topImg, activeImg];
-//       } else {
-//         bottomImg.classList.replace(GALL_BOTTOM, ACTIVE);
-//         activeImg.classList.replace(ACTIVE, GALL_BOTTOM);
-
-//         // swap elements
-//         [activeImg, bottomImg] = [bottomImg, activeImg];
-//       }
-//       break;
-//     }
-//     default:
-//       console.log('NO_EVENTS_EXIST');
-//   }
-// });
-
-// GALLERY.addEventListener('mouseover', (ele) => {
-//   const target = ele.target;
-//   // console.log('MOUSE_OVER:::TARGET', target)
-//   let isActive = target.parentElement.classList.contains('active');
-//   if (target.tagName !== 'IMG' || target.tagName === undefined) return;
-
-//   if (!isActive) {
-//     // console.log('OPACITY-100 ADDED')
-//     target.classList.replace('opacity-50', 'opacity-100');
-//   };
-// });
-
-// for (img of GALLERY_IMAGES) {
-//   img.addEventListener('mouseleave', (ele) => {
-//     const target = ele.target;
-
-//     if (target.tagName !== 'IMG' || target.tagName === undefined) return;
-
-//     const topImg = target.parentElement.classList.contains(GALL_TOP);
-//     const bottomImg = target.parentElement.classList.contains(GALL_BOTTOM);
-
-//     if (topImg || bottomImg) target.classList.replace('opacity-100', 'opacity-50');
-
-//   });
-// }
+});
 // // })
