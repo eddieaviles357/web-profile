8219cf3477418bc5a891f10107d2b208868c48e0
Eduardo Aviles <eduardoaviles357@yahoo.com>
2024-06-03 21:01:36 -0700

    fixed issue when user clicks on hamburger menu. When user window gets resized over 600px causes Menu to disappear.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
-// // window.addEventListener('load', () => {
 window.addEventListener('DOMContentLoaded', (evt) => {
+    let windowWidth = window.innerWidth;
+    let isPhoneSize;
 
-    // SCROLL FUNCTIONALITY
+    setWindowWidth();
+
+
+    /***************************************************
+    *********** RESIZE EVENT LISTENER *****************
+    ****************************************************/
+    window.addEventListener('resize', setWindowWidth);
+
+
+    /***************************************************
+    ******* SCROLL EVENT, INTERSECTION OBSERVER ********
+    ****************************************************/
     const options = {
         root: null,
         threshold: 0,
@@ -26,28 +38,69 @@ window.addEventListener('DOMContentLoaded', (evt) => {
 
     for (let footerSection of footerSections) {
         observer.observe(footerSection);
-    } // END SCROLL FUNCTIONALITY
+    }
 
 
+    /***************************************************
+    *********** NAV MENU FUNCTIONALITY *****************
+    ****************************************************/
+    // NAV MENU FUNCTIONALITY
+    const navMenu = document.querySelector('.nav-menu');
+
+    // CONVERT TO ARRAY SO WE CAN MAP CHILD ELEMENTS
+    const navChilds = Array.from(navMenu.children); 
+    let isNavClicked = false;
 
 
+    let menuClick = e => {
+        if (!isPhoneSize) return; 
+        isNavClicked = !isNavClicked;
+        const target = e.target;
+
+        // console.dir(target);
+        console.log(`%cisNavClicked: ${isNavClicked}`, 'color: yellow; font-size: 1.4rem');
+        switch (target.tagName) {
+            case 'UL': 
+                console.log(`%ctarget tagname === ${target.tagName}`, 'color: green; font-size: 1.4rem ');
+                break;
+            case 'LI':
+                console.log(`%ctarget tagName === ${target.tagName}`, 'color: blue; font-size: 1.4rem')
+            default:
+                console.log('default');
+        }
+        
+
+        navMenuToggleStyles();
+    }
+
+    navMenu.addEventListener('click', menuClick );
 
 
+    /***************************************************
+    *********** UTILITY FUNCTIONS **********************
+    ****************************************************/
+    // nav
+    function navMenuToggleStyles() {
+        navMenu.classList.toggle('active-phone-nav');
+        navChilds.map( child => child.classList.toggle( 'active-phone__children') );
+    }
+    // default nav
+    function removeNavStyles() {
+        navMenu.classList.remove('active-phone-nav');
+        navChilds.map( child => child.classList.remove( 'active-phone__children') );
+    }
+    // resize
+    function setWindowWidth() {
+        windowWidth = window.innerWidth;
+        console.log('WINDOW WIDTH: ', windowWidth);
+        windowWidth <= 600 ? isPhoneSize = true : isPhoneSize = false;
+        
+        // IF USER DECIDES TO CHANGE PHONE WIDTH TO LANDSCAPE OR WINDOW RESIZED BIGGER
+        if(!isPhoneSize || isPhoneSize === 'undefined') {
+            removeNavStyles()
+        }
+    }
 
-    
 });
-// NAV MENU FUNCTIONALITY
-const navMenu = document.querySelector('.nav-menu');
-// CONVERt TO ARRAY SO WE CAN MAP CHILD ELEMENTS
-const navChilds = Array.from(navMenu.children); 
-let isNavClicked = false;
-let windowWidth; // IF WIDTH IS NOT PHONE WIDTH IGNORE MENU CLICKS
 
-let menuClick = e => {
-    navMenu.classList.toggle('active-phone-nav');
-    navChilds.map( child => child.classList.toggle( 'active-phone__children') );
-}
 
-navMenu.addEventListener('click', menuClick );
-
-// // })
